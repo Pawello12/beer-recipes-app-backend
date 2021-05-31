8b41e252751a501b4e01410f7e680e0188a8a6c7
Pawel Switalski <pawelswitalski94@gmail.com>
2021-05-31 12:41:13 +0200

    Initial commit - create strapi app

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '990a9743c140865196eb10651c456eea'),
+    },
+  },
+});
